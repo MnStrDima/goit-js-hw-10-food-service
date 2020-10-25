c4c4df9e235f56bb5aca0ce5ae5c540c42546bf7
Dmitry Medychenko <64079998+MnStrDima@users.noreply.github.com>
2020-10-25 12:23:58 +0200

    Добавляет переменную для ключа темы

--- src/js/task.js
+++ src/js/task.js
@@ -6,10 +6,14 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+const THEMEKEY = 'theme';
+
 const menuContainerRef = document.querySelector('.js-menu');
 const themeSwitcherRef = document.querySelector('#theme-switch-toggle');
 const bodyRef = document.body;
+
 setDarkTheme();
+
 const createMenuMarkup = menuListTpl(menuList);
 menuContainerRef.insertAdjacentHTML('beforeend', createMenuMarkup);
 
@@ -18,20 +22,20 @@ themeSwitcherRef.addEventListener('change', onThemeSwitcherClick);
 function onThemeSwitcherClick(e) {
   if (e.target.checked) {
     bodyRef.classList.replace(Theme.LIGHT, Theme.DARK);
-    localStorage.setItem('theme', Theme.DARK);
+    localStorage.setItem(THEMEKEY, Theme.DARK);
   }
   if (!e.target.checked) {
     bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
-    localStorage.setItem('theme', Theme.LIGHT);
+    localStorage.setItem(THEMEKEY, Theme.LIGHT);
   }
 }
 
 function setDarkTheme() {
-  if (localStorage.getItem('theme') === Theme.DARK) {
+  if (localStorage.getItem(THEMEKEY) === Theme.DARK) {
     themeSwitcherRef.checked = true;
     bodyRef.classList.add(Theme.DARK);
   }
-  if (localStorage.getItem('theme') === Theme.LIGHT) {
+  if (localStorage.getItem(THEMEKEY) === Theme.LIGHT) {
     themeSwitcherRef.checked = false;
     bodyRef.classList.add(Theme.LIGHT);
   }
